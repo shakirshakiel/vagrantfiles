3f732e7868194fb42813294b7d63278210031caf
Shakir Shakiel <shakirshakiel@gmail.com>
2020-07-08 18:39:31 +0530

    Use adoptjdk 11 for zookeeper

--- centos/zookeeper/run_script.sh
+++ centos/zookeeper/run_script.sh
@@ -1,5 +1,14 @@
 yum update -y
-yum install -y java-1.8.0-openjdk.x86_64
+cat <<'EOF' > /etc/yum.repos.d/adoptopenjdk.repo
+[AdoptOpenJDK]
+name=AdoptOpenJDK
+baseurl=http://adoptopenjdk.jfrog.io/adoptopenjdk/rpm/centos/$releasever/$basearch
+enabled=1
+gpgcheck=1
+gpgkey=https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public
+EOF
+yum install -y adoptopenjdk-11-hotspot.x86_64
+
 wget http://apachemirror.wuchna.com/zookeeper/zookeeper-3.6.1/apache-zookeeper-3.6.1-bin.tar.gz
 tar xzvf apache-zookeeper-3.6.1-bin.tar.gz
 chown -R vagrant:vagrant apache-zookeeper-3.6.1-bin
